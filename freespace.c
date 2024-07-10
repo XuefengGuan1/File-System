838836d574031f257e54662944307c8e69484eda
Xuefengguan1 <xuefengguan246@gmail.com>
2024-07-10 12:12:04 -0700

    changed nothing

--- freespace.c
+++ freespace.c
@@ -1,6 +1,6 @@
 /**************************************************************
-* Class::  CSC-415-02 Spring 2024
-* Name::Inderpaul Bhander
+* Class::  CSC-415-0# Spring 2024
+* Name::
 * Student IDs::
 * GitHub-Name::
 * Group-Name::
@@ -31,39 +31,31 @@ void initializeFreeSpace(Freespace *fs) {
     fs->fat[2] = -1;
     fs->freeBlocks = fs->totalBlocks-3;
 }
-
-int allocateBlocks(uint64_t *block_numbers, uint64_t count) {
-    uint64_t allocated_count = 0;
-
-    for (uint64_t i = 0; i < fs->totalBlocks && allocated_count < count; i++) {
-        if (fs->fat[i] == FREE) {
-            block_numbers[allocated_count++] = i;
+int allocateBlock() {
+    for (int i = 0; i < fs->totalBlocks; i++) {
+        if (fs->fat[i] == FREE) { 
+            fs->fat[i] = -1;  
             fs->freeBlocks--;
+            return i;
         }
     }
-
-    for (uint64_t i = 0; i < count; i++) {
-        fs->fat[block_numbers[i]] = (i == count - 1) ? END_OF_CHAIN : block_numbers[i + 1];
-        fs->freeBlocks--;
-    }
-
-    return 0;  
+    fprintf(stderr, "No free blocks available\n");
+    return -1; 
 }
 
-void freeBlocks(uint64_t start_block) {
-    uint64_t current_block = start_block;
-    while (current_block != END_OF_CHAIN) {
-        uint64_t next_block = fs->fat[current_block];
-        fs->fat[current_block] = FREE;
-        current_block = next_block;
-        fs->freeBlocks++;
+void freeBlock(uint64_t blockNumber) {
+    if (blockNumber < 0 || blockNumber >= fs->totalBlocks) {
+        fprintf(stderr, "Invalid block number\n");
+        return;
     }
+    fs->fat[blockNumber] = FREE;  
+    fs->freeBlocks++;
 }
 
-bool isBlockFree(uint64_t block_number) {
-    if (block_number < 0 || block_number >= fs->totalBlocks) {
+bool isBlockFree(uint64_t blockNumber) {
+    if (blockNumber < 0 || blockNumber >= fs->totalBlocks) {
         fprintf(stderr, "Invalid block number\n");
         return false;
     }
-    return fs->fat[block_number] == FREE;
+    return fs->fat[blockNumber] == FREE;
 }
