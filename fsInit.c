c8ee5a5824c68f7987eb60901eecb6a69161fde9
jgard <jgard@sfsu.edu>
2024-07-10 14:37:53 -0700

    had 2 of the same function in fsInit.c, erased one

--- fsInit.c
+++ fsInit.c
@@ -45,29 +45,6 @@ int initFileSystem(uint64_t numberOfBlocks, uint64_t blockSize)
     return 0;
 }
 
-struct VolumeControlBlock vcb;
-
-int initFileSystem(uint64_t numberOfBlocks, uint64_t blockSize)
-{
-    printf("Initializing File System with %ld blocks with a block size of %ld\n", numberOfBlocks, blockSize);
-    /* TODO: Add any code you need to initialize your file system. */
-
-    vcb.blockSize = blockSize;
-    vcb.volumeSignature = 0xFFFF;  // unique signature
-    vcb.rootDirectoryLocation = 2; // block 0 is vcb and block 1 is fat
-    vcb.volumeSize = numberOfBlocks * blockSize;
-    vcb.fatTableLocation = 1; // FAT is block 1, vcb is 0, root is 2
-
-    // write VCB to disk
-    if (LBAwrite(&vcb, 1, 0) != 1)
-    {
-
-        printf("Error: Failed to write VCB to disk\n");
-    }
-
-    return 0;
-}
-
 void exitFileSystem()
 {
     printf("System exiting\n");
