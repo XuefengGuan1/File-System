c602ab63e21da9b769e7e2e6620f11cc9b864d62
jgard <jgard@sfsu.edu>
2024-07-10 21:26:53 -0700

    added start partition system

--- fsInit.c
+++ fsInit.c
@@ -32,7 +32,15 @@ int initFileSystem(uint64_t numberOfBlocks, uint64_t blockSize)
     printf("Initializing File System with %ld blocks with a block size of %ld\n", numberOfBlocks, blockSize);
     /* TODO: Add any code you need to initialize your file system. */
 
-    unsigned int volumeSize = numberOfBlocks * blockSize;
+    uint64_t volumeSize = numberOfBlocks * blockSize;
+    char *filename = "File-System";
+
+    // start
+    if(startPartitionSystem(filename, &volumeSize, &blockSize) != 0) {
+
+        printf("Error: Failed to start");
+        return -1;
+    }
 
     // alocate memory for vcb pointer
     struct VolumeControlBlock *vcbPtr = malloc(sizeof(struct VolumeControlBlock));
