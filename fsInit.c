06397676d23161fff1768a46501644178fae0560
jgard <jgard@sfsu.edu>
2024-07-10 21:16:37 -0700

    chnages to vcb initialzation

--- fsInit.c
+++ fsInit.c
@@ -32,6 +32,8 @@ int initFileSystem(uint64_t numberOfBlocks, uint64_t blockSize)
     printf("Initializing File System with %ld blocks with a block size of %ld\n", numberOfBlocks, blockSize);
     /* TODO: Add any code you need to initialize your file system. */
 
+    unsigned int volumeSize = numberOfBlocks * blockSize;
+
     // alocate memory for vcb pointer
     struct VolumeControlBlock *vcbPtr = malloc(sizeof(struct VolumeControlBlock));
 
@@ -58,6 +60,16 @@ int initFileSystem(uint64_t numberOfBlocks, uint64_t blockSize)
         	free(vcbPtr);
         	return 0;
     	}
+
+    createRootDir(blockSize);
+
+    // VCB initialization
+    if(initialization(volumeSize, blockSize) != 0) {
+
+        printf("Error: Failed to initialize VCB");
+        return -1;
+    }
+
     return 0;
 }
 
