ab3dbdf8d4beb3d45e3eec2a76f8a18e424b6e7b
Inderpaul-Bhander <ibhander@gmail.com>
2024-07-10 21:33:12 -0700

    add free space call.

--- vcb.c
+++ vcb.c
@@ -2,10 +2,13 @@
 #include "vcb.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <fsLow.h>
+#include "fsLow.h"
+#include "freespace.h"
+#include "directoryEntry.h"
 
 #define VOLUME_SIG 0xFFFF
 
+extern Freespace *fs;
 struct VolumeControlBlock vcb;
 
 int initialization(uint64_t volumeSize, uint64_t blockSize) {
@@ -15,7 +18,11 @@ int initialization(uint64_t volumeSize, uint64_t blockSize) {
    	vcb.rootDirectoryLocation = 2; // block 0 is vcb and block 1 is fat
    	vcb.volumeSize = volumeSize;
    	vcb.fatTableLocation = 1; // FAT is block 1, vcb is 0, root is 2
-
+   
+    fs->totalBlocks=volumeSize;
+    fs->fat = (uint32_t*)malloc(volumeSize*sizeof(uint32_t));
+    initializeFreeSpace(fs);
+   
    	// write VCB to disk
    	if (LBAwrite(&vcb, 1, 0) != 1)
 	{
