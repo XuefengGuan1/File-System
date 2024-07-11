3fb4f0869221ed49a3eef8dfd9c0027c4de12063
Xuefengguan1 <xuefengguan246@gmail.com>
2024-07-10 21:07:44 -0700

    added LBAwrite

--- directoryEntry.c
+++ directoryEntry.c
@@ -14,11 +14,12 @@ void createRootDir(int blockSize)
     {
         entries[i].isOccupied = 0;
     }
-    //Allocating memory for the blocks that are assigned to file/dir in freespace
-    //first element of block_numbers will be the start location of any given file
-    //or dir. block_numnbers can be used for LBA write of given file or dir.
-    uint64_t *block_numbers = (uint64_t*)malloc(blockNeeded * sizeof(uint64_t));
-    if (block_numbers == NULL || allocateBlocks(block_numbers, blockNeeded) != 0) {
+    // Allocating memory for the blocks that are assigned to file/dir in freespace
+    // first element of block_numbers will be the start location of any given file
+    // or dir. block_numnbers can be used for LBA write of given file or dir.
+    uint64_t *block_numbers = (uint64_t *)malloc(blockNeeded * sizeof(uint64_t));
+    if (block_numbers == NULL || allocateBlocks(block_numbers, blockNeeded) != 0)
+    {
         free(block_numbers);
         printf("Error: Failed to allocate blocks for root directory\n");
         return;
@@ -30,13 +31,8 @@ void createRootDir(int blockSize)
     time(&entries[0].creationTime);
     time(&entries[0].modificationTime);
     time(&entries[0].accessTime);
-<<<<<<< HEAD
-    entries[0].size = sizeof(rootDir);
-    // entries[0].location = location;
-=======
     entries[0].size = byteNeeded;
     entries[0].location = block_numbers[0];
->>>>>>> e53dfbe96e04a5a788488706134d1ec771fbe3a3
 
     // This only works for the root directory, child directory needs to point to its parent
     //  Init the ..
@@ -47,14 +43,12 @@ void createRootDir(int blockSize)
     time(&entries[1].modificationTime);
     time(&entries[1].accessTime);
     entries[1].size = byteNeeded;
-<<<<<<< HEAD
-    // entries[1].location = location;
-
-    LBAread (&entries, blockNeeded, );
-
-=======
     entries[1].location = block_numbers[0];
->>>>>>> e53dfbe96e04a5a788488706134d1ec771fbe3a3
+
+    for (int i = 0; i < DIRECTORY_ENTRY_NUMBER; i++)
+    {
+        LBAwrite(&entries, 1, block_numbers[i]);
+    }
 }
 
 // rootDir = createDir(50, NULL);
