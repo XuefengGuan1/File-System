2416e4a7ca9fbf77f112cbb86e2dc64bf9180048
Inderpaul-Bhander <ibhander@gmail.com>
2024-07-10 20:10:50 -0700

    Should handle saving FAT to disk and if Fat is multiple blocks

--- directoryEntry.c
+++ directoryEntry.c
@@ -13,9 +13,15 @@ void createRootDir(int blockSize)
     {
         entries[i].isOccupied = 0;
     }
-    // change later
-    // location = allocateBlocks();
-
+    //Allocating memory for the blocks that are assigned to file/dir in freespace
+    //first element of block_numbers will be the start location of any given file
+    //or dir. block_numnbers can be used for LBA write of given file or dir.
+    uint64_t *block_numbers = (uint64_t*)malloc(blockNeeded * sizeof(uint64_t));
+    if (block_numbers == NULL || allocateBlocks(block_numbers, blockNeeded) != 0) {
+        free(block_numbers);
+        printf("Error: Failed to allocate blocks for root directory\n");
+        return;
+    }
     // Init the .
     strcpy(entries[0].name, ".");
     entries[0].isDirect = 1;
@@ -24,7 +30,7 @@ void createRootDir(int blockSize)
     time(&entries[0].modificationTime);
     time(&entries[0].accessTime);
     entries[0].size = byteNeeded;
-    // entries[0].location = location;
+    entries[0].location = block_numbers[0];
 
     //This only works for the root directory, child directory needs to point to its parent
     // Init the ..
@@ -35,7 +41,7 @@ void createRootDir(int blockSize)
     time(&entries[1].modificationTime);
     time(&entries[1].accessTime);
     entries[1].size = byteNeeded;
-    // entries[1].location = location;
+    entries[1].location = block_numbers[0];
 }
 
 // rootDir = createDir(50, NULL);
