205a2ef563d6d8fc2aa40c34e7ab89e712f90f53
Xuefengguan1 <xuefengguan246@gmail.com>
2024-07-15 13:05:03 -0700

    moved endif to the bottom of the file

--- freespace.h
+++ freespace.h
@@ -1,15 +1,16 @@
 #ifndef FREESPACE_H
 #define FREESPACE_H
-#endif
 
-#define FREEBLOCK -1                // A value that indicates a block is free
+#define FREEBLOCK -1            // A value that indicates a block is free
 #define END_OF_CHAIN 0xFFFFFFFE // A value that indicates the end of a chain
 #include <stdbool.h>
 #include "fsLow.h"
 
 int initializeFreeSpace(uint64_t numberOfBlocks, uint64_t blockSize);
-int allocateBlocks(int);
-void freeBlocks(uint64_t start_block);
-bool isBlockFree(uint64_t block_number);
-int saveFAT();
-int loadFAT();
+int allocateBlocks(int numOfBlocksToAllocate);
+int findFreeBlock();
+// void freeBlocks(uint64_t start_block);
+// bool isBlockFree(uint64_t block_number);
+// int saveFAT();
+// int loadFAT();
+#endif
