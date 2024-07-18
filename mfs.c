05a5741ce12ad479cf02c99409caee5cf39aa525
jgard <jgard@sfsu.edu>
2024-07-18 10:23:56 -0700

    working on get/setcwd

--- mfs.c
+++ mfs.c
@@ -1,4 +1,6 @@
 #include "mfs.h"
+#include "directoryEntry.h"
+#include "b_io.h"
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
@@ -82,12 +84,32 @@ int fs_isDir(char *pathname) {
 
 int fs_setcwd(char *pathname) {
 
+    if(pathname == NULL) {
+
+        return -1;
+    }
+
+    if(chdir(pathname) != 0) {
+
+        return -1;
+    }
+
     return 0;
+
 }
 
 char *fs_getcwd(char *pathname, size_t size) {
 
-    return NULL;
+    if(pathname == NULL || size == 0) {
+ 
+        return NULL;
+     }
+    if(getcwd(pathname, size) == NULL) {
+ 
+        return NULL;
+     }
+
+    return pathname;
 }
 
 
