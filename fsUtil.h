edbbda60d75a362158626b8b225717491e5d50da
Inderpaul-Bhander <ibhander@gmail.com>
2024-07-22 11:45:38 -0700

    parse

--- fsUtil.h
+++ fsUtil.h
@@ -2,13 +2,14 @@
 #define FSUTIL_H
 #include <stdbool.h>
 #define MAX_PATH_LENGTH 4096
-#define MAX_TOKENS 128 
+#define MAX_TOKENS 256
+
 typedef struct {
     bool is_absolute;
-    char last_token[MAX_PATH_LENGTH];
-    char tokens[MAX_TOKENS][MAX_PATH_LENGTH];
+    char *tokens[MAX_TOKENS];
     int token_count;
 } Path;
 
+Path parsePath(const char *path);
 
-#endif
+#endif 
